250641b8d9ef1199cba2231352bac4f227437fc9
Slameich-web <SlameACER@mail.ru>
2025-06-30 20:54:29 +0300

    add new pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import { LoginPage } from "./pages/Login";
+import { RegisterPage } from "./pages/Register";
+import { MainPage } from "./pages/Main";
+
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/" element={<MainPage />} />
+    </Routes>
+  );
 }
 
 export default App;
