bd70bec7fa539969d49129563b6ebad6ba4c2957
Slameich-web <SlameACER@mail.ru>
2025-06-30 21:12:00 +0300

    .env vite

--- src/pages/Main/ui/MainPage.tsx
+++ src/pages/Main/ui/MainPage.tsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const MainPage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/login");
+  });
   return <div></div>;
 };
 
