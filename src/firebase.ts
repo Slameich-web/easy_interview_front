4444b4b81eb191882b281a1997d55f46c7c3f41f
Slameich-web <SlameACER@mail.ru>
2025-07-07 21:19:56 +0300

    firebase test

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { addDoc, collection, getDocs, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -10,4 +11,26 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+// Добавление пользователя
+const addUser = async () => {
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      studentId: "ST-2024-001",
+      groupId: "group_fe2024",
+      createdAt: new Date(),
+    });
+    console.log("Документ добавлен с ID: ", docRef.id);
+  } catch (e) {
+    console.error("Ошибка: ", e);
+  }
+};
+addUser();
+const getUsers = async () => {
+  const querySnapshot = await getDocs(collection(db, "users"));
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, " => ", doc.data());
+  });
+};
+getUsers();
